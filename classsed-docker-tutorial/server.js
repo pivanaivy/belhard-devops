8512dd49781613beb94a89102300ee185c0ad561
pivanaivy <asoi720603@gmail.com>
2022-08-04 18:31:55 +0000

    test1

--- classsed-docker-tutorial/server.js
+++ classsed-docker-tutorial/server.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!1'))
 
 app.get('/users', async (req, res) => {
   const users = await db.select().from('users')
